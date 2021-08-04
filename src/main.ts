6c16c3963495f888d93f9bc4f6518d81cee075f5
OleksandrDemian <oleksandrovsky@gmail.com>
2021-08-04 19:48:55 +0200

    Move back to fastify
    Express worked as expected. Try again with Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './controllers/app/app.module';
 
 async function bootstrap() {
-  // todo: move to fastify
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
-  await app.listen(process.env.PORT || 3099);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      cors: true,
+    },
+  );
+  await app.listen(process.env.PORT || 3099, '0.0.0.0');
 }
 bootstrap();
