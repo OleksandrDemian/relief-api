8fce32df96579770064924f8e10af4a57bb7cd39
OleksandrDemian <oleksandrovsky@gmail.com>
2021-08-04 19:41:46 +0200

    move to express
    App is not running on Heroku, try with Express adapter instead of Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './controllers/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-    {
-      cors: true,
-    },
-  );
+  // todo: move to fastify
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   await app.listen(process.env.PORT || 3099);
 }
 bootstrap();
