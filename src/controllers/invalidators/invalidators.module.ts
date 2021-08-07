28b0b2780541fc2990865a82473ef6ca91b43ee2
OleksandrDemian <oleksandrovsky@gmail.com>
2021-08-07 19:25:15 +0200

    inject mongoose into invalidators

--- src/controllers/invalidators/invalidators.module.ts
+++ src/controllers/invalidators/invalidators.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { InvalidatorsService } from './invalidators.service';
 import { InvalidatorsController } from './invalidators.controller';
+import { MongooseModule, SchemaFactory } from '@nestjs/mongoose';
+import { Test } from '../tests/entities/test.entity';
 
 @Module({
   controllers: [InvalidatorsController],
-  providers: [InvalidatorsService]
+  providers: [InvalidatorsService],
+  // todo: same as tests, create shared module to export once all the mongodb models
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Test.name,
+        schema: SchemaFactory.createForClass(Test),
+      },
+    ]),
+  ],
 })
 export class InvalidatorsModule {}
